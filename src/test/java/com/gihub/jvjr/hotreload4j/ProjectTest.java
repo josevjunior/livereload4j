c2ca2f46d66cfad501a460d3cf3e044d1316e54c
Jose Vicente N. da Costa Junior <josev.junior@outlook.com>
2022-07-29 14:39:04 -0300

    Test preconditions

--- src/test/java/com/gihub/jvjr/hotreload4j/ProjectTest.java
+++ src/test/java/com/gihub/jvjr/hotreload4j/ProjectTest.java
@@ -4,18 +4,25 @@ package com.gihub.jvjr.hotreload4j;
 import io.github.josevjunior.livereload4j.Project;
 import java.nio.file.Paths;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 
 public class ProjectTest {
     
     @Test
     public void testSourcePathDiscovery() throws Exception {
+        
+        Assume.assumeNotNull(System.getProperty("user.dir"));
+        
         Project project = Project.determineProject();
         Assert.assertEquals(Paths.get(System.getProperty("user.dir"), "src/main/java"), project.getSourcePath().toAbsolutePath());
     }
     
     @Test
     public void testResourcePathDiscovery() throws Exception {
+        
+        Assume.assumeNotNull(System.getProperty("user.dir"));
+        
         Project project = Project.determineProject();
         Assert.assertEquals(Paths.get(System.getProperty("user.dir"), "src/main/resources"), project.getResourcePath().toAbsolutePath());
     }
